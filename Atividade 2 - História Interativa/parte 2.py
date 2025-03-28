29a26dc286c215e51324a787644df4e7bbe68c6f
lipeinho <edirfelipealves@gmail.com>
2025-03-28 15:43:49 -0300

    alteração by: seandro lordeiro

---
+++ Atividade 2 - História Interativa/parte 2.py
@@ -0,0 +1,14 @@
+#edição por: Leandro Cordeiro
+
+nome = 'Kaio'
+ano_atual = "2025"
+ano_antigo = "350 BCE"
+cidade = 'Grécia'
+polis = 'Atenas'
+filosofo_1 = 'Aristóteles'
+filosofo_2 = 'Platão'
+
+print(f'O Historiador {nome} estava viajando pelas ruas da {cidade} e foi transportado para o passado. Quando percebeu o que aconteceu, notou que estava em {polis} e se encontrou com {filosofo_1} e {filosofo_2}, que estavam em um "duelo de ideias". Ao notar a presença do homem estranho, começaram a questioná-lo sobre onde veio.')
+print(f'{filosofo_1}: {filosofo_2} notas como aquele homem, se veste de forma engraçada?')
+print(f'{filosofo_2}: Sim {filosofo_1}, acredito eu que se trata de um estrangeiro, mas nem em Atlântida vi alguém se vestindo de forma parecida')
+print(f'{filosofo_1}: Sim, pelos deuses! Vamos questioná-lo, quem sabe que conhecimentos estrangeiros ele possui!')
