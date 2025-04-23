f18ab28a2ab93399f1a37371b4ec6270fc94468e
Kaio Konká <kaiojosedonascimento@gmail.com>
2025-04-23 13:42:25 -0300

    reestruturação ex 01

--- Atividade 2 - Funções/ex 01.py
+++ Atividade 2 - Funções/ex 01.py
@@ -4,47 +4,41 @@ task_list = []
 sorted_list = []
 order = False
 
-tasks_qtd = int(input('Quantas tarefas você tem para fazer?: '))
-print('')
-
-for i in range(tasks_qtd):
+def add_tasks():
+    global task_list
     
-    task_dict = {}
-    key1 = 'Tarefa'
-    key2 = 'Descricao'
-    key3 = 'Prazo'
-    key4 = 'Conclusao'
-
     task = input('Qual o nome da tarefa?: ')
     description = input('O que você irá fazer nessa tarefa?: ')
-    date = int(input('Quantos dias você tem para finalizar essa tarefa?: '))
+    deadline = int(input('Quantos dias você tem para finalizar essa tarefa?: '))
     concluded = False
-
-    task_dict[key1] = task
-    task_dict[key2] = description
-    task_dict[key3] = date
-    task_dict[key4] = concluded
     print('')
+        
+    task_list.append({
+        'task': task,
+        'description': description,
+        'deadline': deadline,
+        'concluded': concluded 
+    })
 
-    task_list.append(task_dict)
-
-while True:
-    update =  input('Você finalizou alguma tarefa? (sim / não): ')
+    print(f'A tarefa "{task}" foi adicionada a lista!')
     print('')
-
-    if update ==  'sim':
-        wich_task = input('Qual o nome da tarefa que você finalizou?: ')
-        print('')
-        found = False
-        repeated = False
-        for i in task_list:
-            if i[key1] == wich_task and i[key4] == False:
-                i[key4] = True
-                found = True
+        
+def end_tasks():
+    global task_list   
+    
+    wich_task = input('Qual o nome da tarefa que você finalizou?: ')
+    print('')
+    found = False
+    repeated = False
+    
+    for i in task_list:
+        if i['task'] == wich_task and i['concluded'] == False:
+            i['concluded'] = True
+            found = True
                 
-            elif i[key1] == wich_task and i[key4] == True:
-                found = True
-                repeated = True
+        elif i['task'] == wich_task and i['concluded'] == True:
+            found = True
+            repeated = True
 
         if found == True and repeated == False:
            print(f'A tarefa "{wich_task}" foi marcada como concluída!')
@@ -55,37 +49,45 @@ while True:
         else:
             print(f'A tarefa "{wich_task}" não existe!')
             print('')
-
-    elif update == 'não':
-        print('Certo, você não deseja finalizar mais nenhuma tarefa!')
-        print('')
-        break
-
-    else:
-        print(f'o comando "{update}" não existe!')
-        print('')
-
-while True:
-    sort = input('Você deseja organizar as tarefas pelo prazo? (sim / não): ')
+            
+def sort_tasks():
+    global order
+    global sorted_list
+    
+    sorted_list = sorted(task_list, key=lambda v: v['deadline'])
+    print('A sua lista de tarefas foi organizada!')
     print('')
-
-    if sort == 'sim':
-        sorted_list = sorted(task_list, key=lambda v: v[key3])
-        print('A sua lista de tarefas foi organizada!')
-        print('')
-        order = True
-        break
-    elif sort == 'não':
-        print('Certo, você não deseja organizar sua lista de tarefas!')
-        print('')
-        break
+    order = True 
+    
+def dump_tasks():
+    if order == False:
+        with open("task_list.json", "w") as file:
+            json.dump(task_list, file, indent=4)
     else:
-        print(f'O comando "{sort}" não existe!')
+        with open("task_list.json", "w") as file:
+            json.dump(sorted_list, file, indent=4)
+            
+def main():
+    while True:
+        action = input('Escolha uma ação: \nA. Adicionar tarefa\nF. Finalizar Tarefa\nO. Organizar Lista\nS. Sair\n').upper()
         print('')
-
-if order == False:
-    with open("task_list.json", "w") as file:
-        json.dump(task_list, file, indent=4)
-else:
-    with open("task_list.json", "w") as file:
-        json.dump(sorted_list, file, indent=4)
+        
+        if action == 'A':
+            add_tasks()
+            print('')
+        elif action == 'F':
+            end_tasks()
+            print('')
+        elif action == 'O':
+            sort_tasks()
+            print('')
+        elif action == 'S':
+            dump_tasks()
+            print('A lista de tarefas foi salva em um arquivo .Json!')
+            print('')
+            break
+        else:
+            print('Este comando não existe! Insira um existente!')
+            print('')
+                   
+main()
